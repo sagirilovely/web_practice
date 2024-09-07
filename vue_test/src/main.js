b9715e82292577cabe59d2e3dc1f2d5af2373e72
sagiri <guowenjie1970@163.com>
2024-09-07 20:41:10 +0800

    router

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+Vue.config.productionTip = false
+//导入并安装VueRouter
+import VueRouter from "vue-router";
+Vue.use(VueRouter)
+//导入配置好的router
+import router from "./router/index"
 
 
 new Vue({
-    el:"#app",
-    components:{
-        App
-    },
-    beforeCreate(){
-            //安装全局事件总线
-            Vue.prototype.$bus=this;
-            //就是给Vue的原型上挂一个bus属性,该属性的类型为vm
-            //使其可以使用$on $off $emit方法
-    },
-    render:createElement => createElement(App)
-})
+  router,//配置一个router项
+  render: h => h(App),
+}).$mount('#app')
