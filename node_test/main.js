cdd0f4b71d191159806510d1c3f91c4422428c22
sagiri <guowenjie1970@163.com>
2024-09-19 23:08:52 +0800

    node

--- node_test/main.js
+++ node_test/main.js
@@ -1,29 +1,10 @@
-let {URL,URLSearchParams} =require('url')
-let http=require('http')
+let fs=require('fs')
 
-let https=require('https')
-
-http.createServer((request,response)=>{
-    response.writeHead(200,{
-        "Content-Type":"json/application;charset=utf-8",
-        "access-control-allow-origin":"*"//表示可以接受任何页面的请求
-    })//设置响应头
-    getData((resData)=>{//执行getData函数并传入一个回调,回调的参数用于接受最终getData收集到的数据
-        response.end(resData)//向客户端发送最终数据
-    })
-
-}).listen(2333,()=>{
-    console.log('网址:http://localhost:2333/')
+let num=0//用于记录将stream分成了几个chunk
+let rs=fs.createReadStream('./11/deathEnd.txt');
+let ws=fs.createWriteStream('./11/22.txt');
+rs.pipe(ws)
+rs.on('end',()=>{
+   
 })
-
-function getData(cb){//定义一个用于去其他服务器获取数据的函数,接受的形参是一个回调函数
-    let resultData=''//用于存储数据
-    https.get('https://qqlykm.cn/api/60s/index?key=zOyNSLMTQdgeacbl7ARSwWuUaF',(res)=>{
-        res.on('data',(chunk)=>{//监听data类型的事件,将chunk流累加到resultData里
-            resultData=resultData+chunk;
-        });
-        res.on('end',()=>{//监听end类型事件,一旦chunk流全部收集完毕,则调用此回调
-            cb(resultData);//将最终收集的数据作为回调函数的参数,执行回调
-        })
-    })
-}
+console.log('hhhh');
--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,56 +1,61 @@
 <template>
-  <div class="container">
-      <div class="left">
-          <router-link to="/Aa">点击切换到Aa</router-link>
-          <router-link to="/Bb">点击切换到Bb</router-link>
-      </div>
-      <div class="right">
-            <div >
-                <router-view></router-view>
-            </div>
-      </div>
-  </div>
+	<div class="container">
+		<div class="left">
+			<router-link to="/Aa">点击切换到Aa</router-link>
+			<router-link to="/Bb">点击切换到Bb</router-link>
+		</div>
+		<div class="right">
+			<div>
+				<router-view></router-view>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Aa from "./pages/Aa.vue"
-import Bb from "./pages/Bb.vue"
-import BbCc from "./pages/BbCc.vue"
-import BbDd from "./pages/BbDd.vue"
+import Aa from "./pages/Aa.vue";
+import Bb from "./pages/Bb.vue";
+import BbCc from "./pages/BbCc.vue";
+import BbDd from "./pages/BbDd.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Aa,Bb,BbCc,BbDd
-  }
-}
+	name: "App",
+	components: {
+		Aa,
+		Bb,
+		BbCc,
+		BbDd,
+	},
+	methods: {},
+};
 </script>
 
 <style>
-.container{
-    position:relative;
-    width: 900px;
-    margin: 0 auto;
+.container {
+	position: relative;
+	width: 900px;
+	margin: 0 auto;
 }
 .container::after {
-    display: block;
-    clear: both;
-    content: "";
+	display: block;
+	clear: both;
+	content: "";
 }
-.left ,.right{
-    position: relative;
-    float: left;
-    width: 300px;
-    height: 500px;
-    background-color: cornflowerblue;
-    color: wheat;
+.left,
+.right {
+	position: relative;
+	float: left;
+	width: 300px;
+	height: 500px;
+	background-color: cornflowerblue;
+	color: wheat;
 }
-.right{
-    background-color: gray;
+.right {
+	background-color: gray;
 }
-.left *{
-    display: block;
-    width: 100%;
-    height: 200px;
+.left * {
+	display: block;
+	width: 100%;
+	height: 200px;
 }
 </style>
